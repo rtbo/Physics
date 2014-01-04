47870c42e55ae0197b243f90302aa5d6bc982928
Remi Thebault <remi.thebault@inergyautomotive.com>
2014-01-04 18:06:19 +0100

    add configuration for detecting constexpr and literals availability

--- include/physics/Config.hpp
+++ include/physics/Config.hpp
@@ -11,6 +11,38 @@
 #define PHYSICS_CONFIG_HPP
 
 
+// uncomment the following line if your compiler do not support constexpr
+// #define PHYSICS_NO_CONSTEXPR
+
+// uncomment the following line if your compiler do not support user defined literals
+// #define PHYSICS_NO_LITERALS
+
+
+#if __cplusplus < 201103L
+
+# ifndef PHYSICS_NO_CONSTEXPR
+#  define PHYSICS_NO_CONSTEXPR
+# endif
+
+# ifndef PHYSICS_NO_LITERALS
+#  define PHYSICS_NO_LITERALS
+# endif
+
+#endif
+
+
+#ifndef PHYSICS_NO_CONSTEXPR
+
+#define PHYSICS_CONSTEXPR constexpr
+#define PHYSICS_INCLASS_CONSTEXPR constexpr
+
+#else
+
+#define PHYSICS_CONSTEXPR inline
+#define PHYSICS_INCLASS_CONSTEXPR
+
+#endif
+
 
 
 #endif // PHYSX_CONFIG_HPP
