0d2a4427b412a9cf988006add00a0e77a11f7cd2
Remi Thebault <remi.thebault@outlook.com>
2014-03-15 16:46:08 +0100

    whitespaces

--- tests/Zero.cpp
+++ tests/Zero.cpp
@@ -1,9 +1,9 @@
 /*
-        Copyright (c) 2013 2014, Plastic Omnium Auto INERGY
-        All rights reserved.
+    Copyright (c) 2013 2014, Plastic Omnium Auto INERGY
+    All rights reserved.
 
-        Copy and redistribution of this program in source or binary forms,
-        with or without modification, are strictly forbidden.
+    Copy and redistribution of this program in source or binary forms,
+    with or without modification, are strictly forbidden.
 */
 
 #include "ph/Physics.hpp"
