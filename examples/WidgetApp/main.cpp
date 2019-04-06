870e91c7b6a21087f88515ef1364083fb2818407
Remi Thebault <remi.thebault@gmail.com>
2019-04-06 11:25:28 +0200

    designer plugin works

--- examples/WidgetApp/main.cpp
+++ examples/WidgetApp/main.cpp
@@ -1,48 +1,14 @@
 
-#include <siui/VolumeSpinBox.hpp>
-#include <siui/PressureSpinBox.hpp>
-#include <siui/EnergyLabel.hpp>
-
+#include "widget.h"
 #include <QtWidgets>
 
 int main (int argc, char *argv[])
 {
     QApplication app { argc, argv };
-    QWidget window;
+    Widget widget;
 
-    window.setWindowTitle("Physics Widget App");
-    auto *layout = new QBoxLayout{ QBoxLayout::LeftToRight, &window };
-
-    auto *vol = new siui::VolumeSpinBox{ &window };
-    vol->setUnit(si::volume::Unit::meter3);
-    vol->setVolume(si::meter3_t{ 12 });
-
-    auto *pre = new siui::PressureSpinBox{ &window };
-    pre->setUnit(si::pressure::Unit::bar);
-    pre->setPressure(si::bar_t{ 4 });
-
-    auto *enr = new siui::EnergyLabel{ &window };
-    enr->setUnit(si::energy::Unit::kilojoule);
-
-
-    auto *lbl1 = new QLabel{ " at ", &window };
-    auto *lbl2 = new QLabel{ " represent ", &window };
-
-    layout->insertWidget(0, vol);
-    layout->insertWidget(1, lbl1);
-    layout->insertWidget(2, pre);
-    layout->insertWidget(3, lbl2);
-    layout->insertWidget(4, enr);
-
-    auto update = [&] () {
-        enr->setEnergy( vol->volume() * pre->pressure() );
-    };
-
-    QObject::connect(vol, &siui::VolumeSpinBox::volumeChanged, update);
-    QObject::connect(pre, &siui::PressureSpinBox::pressureChanged, update);
-    update();
-
-    window.showNormal();
+    widget.setWindowTitle("Physics Widget App");
+    widget.showNormal();
 
     return app.exec();
 }
---
+++ examples/WidgetApp/widget.cpp
@@ -0,0 +1,23 @@
+#include "widget.h"
+#include "ui_widget.h"
+
+Widget::Widget(QWidget *parent) :
+    QWidget(parent),
+    ui(new Ui::Widget)
+{
+    ui->setupUi(this);
+
+    auto update = [&] () {
+        ui->energyLabel->setEnergy( ui->volumeSpinBox->volume() * ui->pressureSpinBox->pressure() );
+    };
+
+    QObject::connect(ui->volumeSpinBox, &siui::VolumeSpinBox::volumeChanged, update);
+    QObject::connect(ui->pressureSpinBox, &siui::PressureSpinBox::pressureChanged, update);
+    update();
+
+}
+
+Widget::~Widget()
+{
+    delete ui;
+}
